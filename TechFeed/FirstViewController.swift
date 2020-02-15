d2cf6d7dd9bc67df8d9d98113f78fd7b68bb6291
cod3breaker <ravisherath@gmail.com>
2020-02-15 20:15:03 +1100

    Initial Commit

---
+++ TechFeed/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  TechFeed
+//
+//  Created by Sunimal Herath on 15/2/20.
+//  Copyright © 2020 Sunimal Herath. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TechFeed/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  TechFeed
+//
+//  Created by Sunimal Herath on 15/2/20.
+//  Copyright © 2020 Sunimal Herath. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
