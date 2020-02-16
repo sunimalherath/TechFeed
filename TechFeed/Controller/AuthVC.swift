d29308e7f6ac81d652ea1f20507c4ba757c00f71
cod3breaker <ravisherath@gmail.com>
2020-02-16 20:26:09 +1100

    Adding AuthVC

---
+++ TechFeed/Controller/AuthVC.swift
@@ -0,0 +1,18 @@
+//
+//  AuthVC.swift
+//  TechFeed
+//
+//  Created by Sunimal Herath on 16/2/20.
+//  Copyright © 2020 Sunimal Herath. All rights reserved.
+//
+
+import UIKit
+
+class AuthVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
