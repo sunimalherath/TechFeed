2b00076b68e950dcb8dfdc6b5280c83320e102e6
cod3breaker <ravisherath@gmail.com>
2020-02-16 20:35:07 +1100

    Adding LoginVC

---
+++ TechFeed/Controller/LoginVC.swift
@@ -0,0 +1,18 @@
+//
+//  LoginVC.swift
+//  TechFeed
+//
+//  Created by Sunimal Herath on 16/2/20.
+//  Copyright © 2020 Sunimal Herath. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
